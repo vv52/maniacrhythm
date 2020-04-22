4d6388ddaf6fcfbee1cfeef636d4ec2864d2fa0e
Vanja <vv52@humboldt.edu>
2020-04-22 13:16:17 -0700

    Added a slightly modified version of the Conductor class as described by Yu Chao and Graham Tattersall as well as wrote a Notes class that holds a float position read as beat number as well as an array of four boolean values to determine which note lanes will be active.
    I imported two songs of mine into the project, edited so they start exactly 8 beats from the 0 dspTime position. I tested the audio loading and tracking by the Conductor class on my SongManager gameObject by starting the scene and looking at the beat count live in Unity. The song started exactly where it was supposed to and thus I'm feeling relatively confident as the timing was the part I was most worried about! Huge thanks to Yu Chao and Graham Tatters for their incredibly helpful blog posts about utilizing dspTime instead of timeSinceLevelLoad to time rhythm games due to it's direct utilization of the audio sample rate for time calculation.

---
+++ Assets/Scripts/Conductor.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conductor : MonoBehaviour
+{
+    //Song beats per minute
+	//This is determined by the song you're trying to sync up to
+	public float songBpm;
+
+	//The number of seconds for each song beat
+	public float secPerBeat;
+
+	//Current song position, in seconds
+	public float songPosition;
+
+	//Current song position, in beats
+	public float songPositionInBeats;
+
+	//How many seconds have passed since the song started
+	public float dspSongTime;
+
+	//an AudioSource attached to this GameObject that will play the music.
+	public AudioSource musicSource;
+
+	//The offset to the first beat of the song in seconds
+	public float firstBeatOffset;
+
+	//keep all the position-in-beats of notes in the song
+	Notes[] notes = new Notes[4];
+
+	//the index of the next note to be spawned
+	int nextIndex = 0;
+
+	void Start()
+	{
+	    //Load the AudioSource attached to the Conductor GameObject
+	    musicSource = GetComponent<AudioSource>();
+
+	    //Calculate the number of seconds in each beat
+	    secPerBeat = 60f / songBpm;
+
+	    //Record the time when the music starts
+	    dspSongTime = (float)AudioSettings.dspTime;
+
+	    //Start the music
+	    musicSource.Play();
+	}
+
+	void Update()
+	{
+	    //determine how many seconds since the song started
+	    songPosition = (float)(AudioSettings.dspTime - dspSongTime - firstBeatOffset);
+
+	    //determine how many beats since the song started
+	    songPositionInBeats = songPosition / secPerBeat;
+
+	    if (nextIndex < notes.Length && notes[nextIndex].pos < songPositionInBeats)
+		{
+		    //Instantiate( /* Music Note Prefab */ );
+
+		    //initialize the fields of the music note
+
+		    nextIndex++;
+		}
+	}
+}
---
+++ Assets/Scripts/Notes.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour
+{
+	//position in beats from start of song
+    public float pos;
+    public bool[] notes = new bool[] { false, false, false, false };
+
+    public Notes (float posBeats, bool[] activeNotes)
+    {
+    	pos = posBeats;
+    	activeNotes.CopyTo(notes, 0);
+    }
+}
