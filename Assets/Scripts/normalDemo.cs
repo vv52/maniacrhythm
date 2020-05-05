9b266157cc8ed636f4097feefd299e119c413af0
Vanja <vv52@humboldt.edu>
2020-05-04 18:44:17 -0700

    I've officially reached alpha v0.1 of ManiacRhythm. I will continue bug testing and optimizing but this is the version I intend to present for CS 211.
    Development will continue.

---
+++ Assets/Scripts/normalDemo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class normalDemo : MonoBehaviour
+{
+    public void OnClick()
+    {
+    	SceneManager.LoadScene("twb", LoadSceneMode.Single);
+    }
+}
